dbea2085928af3c5d5076ee8fbc9049a3b64d779
Brian <bhuan84497@gmail.com>
2026-07-13 17:15:59 -0400

    Add eventlisteners and numberClicked

--- script.js
+++ script.js
@@ -1,44 +1,43 @@
-function add(a, b) {
-    return a + b;
-}
-
-function subtract(a, b) {
-    return a - b;
-}
-
-function multiply(a, b) {
-    return a * b;
-}
-
-function divide(a, b) {
-    return a / b;
-}
-
 // decides whether it was a number/equal sign, operator or clear that's been clicked
-function whenClicked(e) {
-    const clickedItem = e.target.value;
+function addEventsandParse() {
+    const inputPanel = document.querySelector(".inputPanel");
 
-    if (clickedItem.classList.contains('=') {
+    inputPanel.addEventListener('click', (e) => {
+        if (e.target.classList.contains('clear')) {
+            clearClicked(e);
+        } else if (e.target.classList.contains('op')) {
+            operatorClicked(e);
+        } else if (e.target.classList.contains('num')) {
+            numberClicked(e);
+        }
+    });
+}
 
+// if it was a number, store var and eval if needed
+function numberClicked(e) {
+    const typedPanel = document.querySelector('.typedPanel');
+
+    if (!operator) {
+        var1 = var1 + e.target.textContent;
+        typedPanel.textContent = var1;
+    } else if (operator && var1) {
+        var2 = var2 + e.target.textContent;
+        typedPanel.textContent = typedPanel.textContent + var2;
     }
 }
 
-// if it was a number, store var and eval if needed. If it was equals sign, eval and run clear
-function numberClicked(e) {
-
-}
-
-// if it was an operator, store operator
+// if it was an operator, store operator or clear bin
 function operatorClicked(e) {
-
+    console.log("operatorClicked " + e.target);
 }
 
 // if it was clear, clear everything
 function clearClicked(e) {
-
+    console.log("clearClicked " + e.target);
 }
 
-let firstVar = 0;
+let var1 = '';
+let var2 = '';
 let operator = '';
-let secondVar = 0;
 
+addEventsandParse();
