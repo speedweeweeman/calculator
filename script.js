e52a4c89f2c6d423f8e0044ab275f366764b6845
Brian <bhuan84497@gmail.com>
2026-07-13 11:49:59 -0400

    Architect functions

---
+++ script.js
@@ -0,0 +1,40 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+// decides whether it was a number/equal sign, operator or clear that's been clicked
+function whenClicked(event) {
+    event.target
+}
+
+// if it was a number, store var and eval if needed. If it was equals sign, eval and run clear
+function numberClicked(event) {
+
+}
+
+// if it was an operator, store operator
+function operatorClicked(event) {
+
+}
+
+// if it was clear, clear everything
+function clearClicked(event) {
+
+}
+
+let firstVar = 0;
+let operator = '';
+let secondVar = 0;
+
